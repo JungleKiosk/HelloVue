1b035f6baeaf24c689d38facd5a81bc661ab4f07
JungleKiosk <eso.fra24@gmail.com>
2023-06-07 02:23:40 +0200

    setting project: add layout

---
+++ src/components/LoVue.vue
@@ -0,0 +1,55 @@
+<script>
+
+export default {
+  name: 'LoVue',
+  data() {
+
+    return {
+    }
+  }
+};
+
+</script>
+
+<template>
+  <div class="container">
+    <div class="row">
+
+      <div class="col-6">
+        <h1>Say</h1>
+        <h1>Hello <span>Vue</span></h1>
+        <h1>10 times</h1>
+
+        <h2></h2>
+      </div>
+
+      <div class="col-6">
+        <div>
+          <a href="https://vitejs.dev" target="_blank">
+            <img src="/vite.svg" class="logo" alt="Vite logo" />
+          </a>
+          <a href="https://vuejs.org/" target="_blank">
+            <img src="../assets/vue.svg" class="logo vue" alt="Vue logo" />
+          </a>
+        </div>
+      </div>
+
+    </div>
+
+
+
+  </div>
+</template>
+
+<style>
+span {
+  color: #40df98aa;
+}
+
+.logo {
+  height: 6rem;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+</style>
